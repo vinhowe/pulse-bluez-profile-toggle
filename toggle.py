5d3f924ae68d28f735f26a002234be2abfdf9d01
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2020-10-28 18:57:23 -0600

    Initial commit

---
+++ toggle.py
@@ -0,0 +1,17 @@
+import pulsectl
+
+def toggle():
+    pulse = pulsectl.Pulse("profile-switcher")
+
+    default_sink_name = pulse.server_info().default_sink_name
+    profile = default_sink_name.split(".")[-1]
+    card = next(filter(lambda x: "bluez" in x.name, pulse.card_list()))
+    print(dir(card))
+
+    new_profile = "headset_head_unit" if profile == "a2dp_sink" else "a2dp_sink"
+    pulse.card_profile_set(card, new_profile)
+    pulse.close()
+
+
+if __name__ == "__main__":
+    toggle()
